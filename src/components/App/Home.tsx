a28a063a932411c1e825f44befb63c676d1bd785
Mikael Lirbank <mikael@airlab.io>
2018-04-11 19:44:40 -0700

    Fix build

--- src/components/App/Home.tsx
+++ src/components/App/Home.tsx
@@ -1,41 +1,25 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
 import {compose, withProps} from 'recompose';
-import {withStyles} from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 import withCurrentUser from '../../hoc/withCurrentUser';
 import SignIn from './SignIn';
+import styled from 'styled-components';
 
-const styles = () => ({
-  root: {
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    display: 'flex',
-    'justify-content': 'center',
-    'align-items': 'center',
-  },
-  inner: {
-    textAlign: 'center',
-  },
-});
+const Root = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Inner = styled.div`
+  text-align: center;
+`;
 
 export class Home extends React.Component<{
   refetchCurrentUser: Function;
-  classes: any;
   email?: string;
 }> {
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-    email: PropTypes.string,
-    refetchCurrentUser: PropTypes.func.isRequired,
-  };
-
-  static defaultProps = {
-    email: null,
-  };
-
   // The current user is null before sign in, so we need to refetch when sign in has completed
   handleSignInCompleted = () => {
     this.props.refetchCurrentUser();
@@ -43,8 +27,8 @@ export class Home extends React.Component<{
 
   render() {
     return (
-      <div className={this.props.classes.root} style={{position: 'absolute'}}>
-        <div className={this.props.classes.inner}>
+      <Root>
+        <Inner>
           <Typography align="center" variant="display1">
             Hologram Rose
           </Typography>
@@ -60,14 +44,13 @@ export class Home extends React.Component<{
               Signed in as {this.props.email}
             </Typography>
           )}
-        </div>
-      </div>
+        </Inner>
+      </Root>
     );
   }
 }
 
 export default compose(
-  withStyles(styles),
   withCurrentUser,
   withProps((ownProps: any) => {
     return {
