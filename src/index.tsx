9efda7339f709fc46305cd516f4fa1bc7dd20c1e
Mikael Lirbank <mikael@airlab.io>
2018-05-25 21:21:08 -0700

    CircleCI (#5)

--- src/index.tsx
+++ src/index.tsx
@@ -10,5 +10,7 @@ import App from './components/App/App';
 // https://en.wikipedia.org/wiki/Rose_(color)
 // setFavicon('#ff007f', 64);
 
+console.log('new version 5');
+
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 // registerServiceWorker();
